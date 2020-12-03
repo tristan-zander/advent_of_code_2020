1dfa2337f7c4e87a5247285f6f33d3f98c414347
Tristan Zander <knavetnz@gmail.com>
2020-12-02 23:59:42 -0500

    Finished day 2 part 2

--- day2/p1/src/main.rs
+++ day2/p1/src/main.rs
@@ -1,7 +1,16 @@
 use day2::get_inputs;
 fn main() {
     let inputs = get_inputs("../input");
+    let mut valid_passwords = 0;
     for inp in inputs.iter() {
-        //
+        let mut counter = -1; // To account for the fact that split will add an extra element at the end.
+        for _ in inp.password.split(inp.letter) {
+            counter += 1;
+        }
+        if counter >= inp.occurances[0] && counter <= inp.occurances[1] {
+            valid_passwords += 1;
+            continue;
+        }
     }
+    dbg!(valid_passwords);
 }
--- day2/p2/src/main.rs
+++ day2/p2/src/main.rs
@@ -1,3 +1,15 @@
+use day2::*;
+use std::ops::Index;
+
 fn main() {
-    println!("Hello, world!");
+    let inputs = get_inputs("../input");
+    let mut valid_passwords = 0;
+    for inp in inputs.iter() {
+        let index1 = inp.password.as_bytes()[inp.occurances[0] as usize - 1];
+        let index2 = inp.password.as_bytes()[inp.occurances[1] as usize - 1];        
+        if (index1 == inp.letter as u8 || index2 == inp.letter as u8) && index1 != index2 {
+            valid_passwords += 1;
+        }
+    }
+    dbg!(valid_passwords);
 }
